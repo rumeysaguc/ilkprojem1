e828b98ccef5a5a648b98dcc8b159484759079af
Rümeysa <70983764+rumeysaguc@users.noreply.github.com>
2025-05-06 20:32:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-x=10
-y=15
-print(x+y)
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def sifreuret(ctx):
+    elementler = "+-/*!&$#?=@<>1234567890abcdefgh"
+    sifre = ""
+    for i in range(8):
+        sifre += random.choice(elementler)
+    await ctx.send(sifre)
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("")
